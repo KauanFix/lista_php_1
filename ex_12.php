6d7964d63b3517e1073c2548cac51546cbe8e057
KauanFix <kauan_fix@estudante.sesisenai.org.br>
2026-07-08 12:56:59 -0300

    ADD - mais itens ao estoque do ex_12

--- ex_12.php
+++ ex_12.php
@@ -15,6 +15,7 @@
         <button type="submit">Entrar</button>
 
     </form>
+    <br>
 </body>
 </html>
 
@@ -24,7 +25,13 @@ $estoque = [
     ["nome" => "Leite Tirol", "preco" => 4.50],
     ["nome" => "Arroz Branco", "preco" => 24.00],
     ["nome" => "Cebola 1kg", "preco" => 6.90],
-    ["nome" => "Alface/Repolho", "preco" => 9.50],
+    ["nome" => "Macarrão", "preco" => 12.99],
+    ["nome" => "Brinquedo", "preco" => 29.50],
+    ["nome" => "Sofá", "preco" => 199.00],
+    ["nome" => "Maçã", "preco" => 10.50],
+    ["nome" => "Computador", "preco" => 200.00],
+    ["nome" => "Globgogabgalab", "preco" => 67.67],
+    ["nome" => "Boneco", "preco" => 3.50],
 ];
 function analisarProdutos($Produtos){
     $precos = array_column($Produtos, 'preco');
@@ -32,7 +39,9 @@ function analisarProdutos($Produtos){
     $produtoBarato = min($precos);
 
     $mediaPrecos = array_sum($precos) / count($Produtos);
+echo "<br>";
 print_r($Produtos);
+echo "<br>";
 echo "<br>";
     return [$produtoCaro, $produtoBarato, $mediaPrecos];
 }
