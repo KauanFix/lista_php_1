4759502bb29f7eb3477f2e184bdefacafcad5aed
KauanFix <kauan_fix@estudante.sesisenai.org.br>
2026-07-08 12:52:08 -0300

    ex_12

---
+++ ex_12.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mercadinho</title>
+</head>
+<body>
+    <h2>Pesquisa de Produtos</h2>
+
+    <form method="POST">
+        <label for="pesquisa">Produto:</label> 
+        <input type="text" name="pesquisa">
+        <br>
+        <button type="submit">Entrar</button>
+
+    </form>
+</body>
+</html>
+
+<?php
+
+$estoque = [
+    ["nome" => "Leite Tirol", "preco" => 4.50],
+    ["nome" => "Arroz Branco", "preco" => 24.00],
+    ["nome" => "Cebola 1kg", "preco" => 6.90],
+    ["nome" => "Alface/Repolho", "preco" => 9.50],
+];
+function analisarProdutos($Produtos){
+    $precos = array_column($Produtos, 'preco');
+    $produtoCaro = max($precos);
+    $produtoBarato = min($precos);
+
+    $mediaPrecos = array_sum($precos) / count($Produtos);
+print_r($Produtos);
+echo "<br>";
+    return [$produtoCaro, $produtoBarato, $mediaPrecos];
+}
+
+
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") { //if para executar no envio do formulário
+
+    $pesquisa = $_POST["pesquisa"];
+
+    $itens = array_column($estoque, 'nome');
+
+
+    $itemProcurado = "Nenhum Produto Encontrado";
+    $pesquisa = strtolower($pesquisa);
+
+    foreach ($itens as $item){
+        $item = strtolower($item);
+        $similaridade = similar_text($item, $pesquisa, $porcentagem);
+        if($porcentagem > 50){
+            $itemProcurado = $item;
+        }
+    }
+    echo "Pesquisa: $itemProcurado <br>";
+    
+}
+
+
+
+$resultado = analisarProdutos($estoque);
+echo "O produto mais caro é: $resultado[0]<br>";
+echo "O produto mais barato é: $resultado[1]<br>";
+echo "a média de preço é: $resultado[2]<br>";
+
